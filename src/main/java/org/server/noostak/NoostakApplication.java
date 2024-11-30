f26204ca485a8fadfc09c8bba8d9b4a1631cbafc
권장순 <jsoonworld@gmail.com>
2024-12-01 00:34:19 +0900

    feat : init

---
+++ src/main/java/org/server/noostak/NoostakApplication.java
@@ -0,0 +1,13 @@
+package org.server.noostak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoostakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoostakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/server/noostak/NoostakApplicationTests.java
@@ -0,0 +1,13 @@
+package org.server.noostak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoostakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
